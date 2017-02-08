98326aeb017f1522619a3f0fa5df096f5c7a9731
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2017-02-08 18:08:39 -0500

    Update bysite_logistic.py

--- scripts/bysite_logistic.py
+++ scripts/bysite_logistic.py
@@ -47,3 +47,46 @@ groups = diag_scz_cnt.groupby('STUDY')
 def rem_redudant(encoded):
     to_drop = []
     for col in range(1, encoded.shape[1]):
+        if (encoded.ix[:, col - 1] + encoded.ix[:, col]).mean() == 1:
+            to_drop.append(encoded.columns[col])
+    return encoded.drop(to_drop,axis=1)
+
+def logistic(X, y, splits):
+    out = {'auc':[], 'coef':[]}
+    cv_out = StratifiedShuffleSplit(n_splits = splits)
+    cv_in = StratifiedShuffleSplit(n_splits = splits - 1)
+    for train, test in cv_out.split(X, y):
+        clf = Pipeline([
+            ('scale', StandardScaler()),
+            ('lgr', linear_model.LogisticRegressionCV(
+                        cv = cv_in,
+                        penalty = 'l1',
+                        solver = 'liblinear'))])
+        clf.fit(X[train], y[train])
+        out['auc'].append(roc_auc_score(y[test], clf.predict(X[test])))
+        out['coef'].append(clf.named_steps['lgr'].coef_)
+    return np.mean(out['auc']), out['coef']
+
+def reidx(df):
+    df1 = df.copy()
+    df1.index = [x for x in range(df.shape[0])]
+    return df1
+
+results = {}
+
+for name, group in groups:
+    temp_encode = []
+    cvars = group.ix[:, covar_idx(group, covars_1)]
+    for col in range(cvars.shape[1])[:-3]:
+        temp_encode.append(encode(cvars.ix[:, col]))
+    encoded = pd.concat(temp_encode, axis=1)
+    encoded = reidx(rem_redudant(encoded))
+    brain_data = reidx(group.ix[:, :-25])
+    age = reidx(group[['AGE_MRI']])
+    model_data = pd.concat([brain_data, age, encoded], axis=1)
+    try:
+        results[name] = logistic(X = model_data,
+                                 y = ,
+                                 splits = 4)
+    except ValueError:
+        results[name] = 'No variance in population'
