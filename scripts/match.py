675e8c4c9eca7ab1b9c429967da466e14ad73db6
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2017-02-14 17:32:35 -0500

    Update match.py

--- scripts/match.py
+++ scripts/match.py
@@ -4,24 +4,25 @@ import magic
 
 class Match(object):
     def __init__(self, brain, genomic, cognitive, id_var, sep = None):
-        self.brain = brain
+        self.brain = brain,
         self.genomic = genomic,
-        self.cognitive = cognitive
+        self.cognitive = cognitive,
         self.id_var = id_var,
         self.sep = sep
 
     data_store_types = ('Hierarchical Data Format',
                         'ASCII', 'Matlab', 'mat-file')
+
     def inter(self, loi):
         return list(set(loi[0]).intersection(*loi))
 
     def get_matching_ids(self, id_var, b, c, g):
-        id_var_inter = inter([val.columns.values for val in (b, c, g)])
+        id_var_inter = inter([val.columns.values for val in [b, c, g]])
 
         if not id_var_inter[0] == id_var:
             raise Exception("Some input data is missing ID variable")
         else:
-            reduce_n = inter([val[id_var_inter[0]] for val in (b, c, g)])
+            reduce_n = inter([val[id_var_inter[0]] for val in [b, c, g]])
             return reduce_n
 
     def load(self, data):
@@ -40,8 +41,11 @@ class Match(object):
         return data.set_index(id_var).loc(ids)
 
     def out(self):
-        data = {'b': self.load(self.brain), 'c': self.load(self.cognitive),
-                'g': self.load(self.genomic)}
+        data = {
+        'b': self.load(self.brain),
+        'c': self.load(self.cognitive),
+        'g': self.load(self.genomic)
+        }
 
         ids = get_matching_ids(self.id_var, **data)
 
