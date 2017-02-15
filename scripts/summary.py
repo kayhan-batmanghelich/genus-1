9a1f3e7ce23cc77cc6a1628f46c20450b7e9bf33
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2017-02-15 12:42:37 -0500

    Update summary.py

--- scripts/summary.py
+++ scripts/summary.py
@@ -2,30 +2,21 @@ import numpy as np
 import pandas as pd
 
 class Summary(object):
-
-    sex_cols = ('sex', 'gender', 'male', 'female')
-
-    def __init__(self, data):
+    def __init__(self, data, cols):
         self.data = data
+        self.cols = cols
 
     def loc(self):
         locs = []
         cols = {i.lower():i for i in self.data.columns}
         for idx, val in enumerate(self.data.columns):
-            if val.lower() in self.sex_cols:
+            if val.lower() in self.cols:
                 locs.append((val, idx))
-        return locs
-
-    def intermediate(self, locs):
-        locs = [i[1] for i in locs]
-        slice = self.data[locs]
+        loc = [i[1] for i in locs]
+        slice = self.data[loc]
         li = [i for i in slice.values.flatten() if not pd.isnull(i)]
-        num_sex = len(set(li))
-        nan = 0
-        for i in slice.values.flatten():
-            if pd.isnull(i):
-                nan += 1
         rd = {i:li.count(i) for i in li}
-        rd['nan'] = nan
-        rd['nsex'] = num_sex
         return rd
+
+    def fit(self):
+        return self.loc()
