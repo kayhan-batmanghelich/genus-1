e749d7666e248b3d8449a4e2df8a2afa50ef102c
YSanchezAraujo <yoelsanchez006@gmail.com>
2017-02-15 16:41:20 -0500

    update

--- scripts/brain_gradient_logistic.py
+++ scripts/brain_gradient_logistic.py
@@ -27,31 +27,41 @@ def logistic(X, y, splits):
     return results
 
 data_columns = np.genfromtxt('genus/text_files_for_indexing/170_columns.txt', dtype=str)
-covar_columns = np.genfromtxt('genus/text_files_for_indexing/covariate_variables.txt', dtype=str)
+covar_encode = np.genfromtxt('genus/text_files_for_indexing/covars_ecn.txt', dtype=str)
+covar_noencode = np.genfromtxt('genus/text_files_for_indexing/covars_no_ecn.txt', dtype=str)
 id_columns = np.genfromtxt('genus/text_files_for_indexing/id_variables.txt', dtype=str)
 
 # load and index data
 data = pd.read_csv('brain/GENUS_FS_ATLAS_D.csv', low_memory = False)
 data_use = data[data_columns]
-covar_use = data[covar_columns]
+covar_use_encode = data[covar_encode]
+covar_use_noencode = data[covar_noencode]
 id_use = data[id_columns]
 
 # make sure ids are matching and drop nans and duplicates
-covar_id = pd.concat([id_use[['IID']], covar_use], axis=1).dropna().drop_duplicates('IID')
+covar_id1 = pd.concat([id_use[['IID']],
+                       covar_use_noencode],
+                       axis=1).dropna().drop_duplicates('IID')
+covar_id2 = pd.concat([id_use[['IID']],
+                       covar_use_encode],
+                       axis=1).dropna().drop_duplicates('IID')
+cvars = pd.concat([covar_id1, covar_id2], axis=1).dropna()
+cvars1 = cvars[covar_noencode]
+cvars2 = cvars[covar_encode]
 data_id = pd.concat([id_use[['IID']], data_use], axis=1)
-data_id = data_id.set_index('IID', 1).loc[covar_id.IID]
+data_id = data_id.set_index('IID', 1).loc[cvars.IID.ix[:, 0]]
 data_id['IID'] = data_id.index.values
 data_id = data_id.drop_duplicates(subset='IID')
-id_check = np.all(data_id.IID.values == covar_id.IID.values)
-
+id_check = np.all(data_id.IID.values == cvars.IID.ix[:, 0].values)
 if not id_check:
     raise Exception("IDs not the same, cannot continue")
-
 data_id = reidx(data_id.drop('IID', 1))
-covar_id = reidx(covar_id.drop('IID', 1))
+cvars1 = reidx(cvars1)
+cvars2 = reidx(cvars2)
 temp_encode = []
 # first covariate is EstimatedTotalIntraCranialVol
-for col in covar_id.columns[1:]:
-    temp_encode.append(encode(covar_id[col]))
-#temp_encode.append(covar_id.ix[:,0])
+for col in cvars2.columns:
+    temp_encode.append(encode(cvars2[col]))
 encoded_covar = pd.concat(temp_encode, axis=1)
+encoded_covar = remove_redudant(encoded_covar)
+covars = pd.concat([cvars1, encoded_covar], axis=1)
